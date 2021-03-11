f8599cef6d65c3c78bd7fbe361e270b93dd5a594
Gowthami <gowthamiveerapaneni0@gmail.com>
2021-03-11 12:15:13 +0100

    data correction in.sql file

--- Vehicles-Backend/src/main/resources/data.sql
+++ Vehicles-Backend/src/main/resources/data.sql
@@ -1,6 +1,6 @@
 
 insert into vehicle values (1,'Cementvagen 8, 111 11 Sodertalje','Connected','Kalles Grustransport AB','ABC123','YS2R4X20005399401');
-insert into vehicle values (2,'Cementvagen 8, 111 11 Sodertalje','DisConnected','Kalles Grustransport AB','DEF456','VLUR4X20009093588');
+insert into vehicle values (2,'Cementvagen 8, 111 11 Sodertalje','Disconnected','Kalles Grustransport AB','DEF456','VLUR4X20009093588');
 insert into vehicle values (3,'Cementvagen 8, 111 11 Sodertalje','Connected','Kalles Grustransport AB','GHI789','VLUR4X2000909080066');
 insert into vehicle values (4,'Balkvagen 12, 222 22 Stockholm','Connected','Johans Bulk AB','kl0012','YS2R4X20005388011');
 insert into vehicle values (5,'Balkvagen 12, 222 22 Stockholm','Connected','Johans Bulk AB','MN0345','YS2R4X20005387949');
